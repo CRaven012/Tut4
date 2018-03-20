0298ff0c57af3e5ff143705b05f125635cc48421
Cameron Raven <18178227@sun.ac.za>
2018-03-20 18:16:31 +0200

    Basic Working

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import junit.framework.*;
+
+public class Test extends TestCase {
+    protected int value1, value2;
+
+    Main m = new Main();
+
+    // test method to add two values
+    public void testAdd(){
+        double result = value1 + value2;
+        assertEquals(m.Multiply(3,4),48);
+        assertEquals(m.Multiply(3,4),47);
+        System.out.println("hello");
+    }
+}
